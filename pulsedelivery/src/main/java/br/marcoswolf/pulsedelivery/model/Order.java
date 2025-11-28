486fed839d8eec865f9b1c080bab6782cc162851
Marcos Vinícios <viniciosramos.dev@gmail.com>
2025-11-27 23:09:22 -0300

    chore: alteração no docker-compose, pom e no application.properties

--- pulsedelivery/src/main/java/br/marcoswolf/pulsedelivery/model/Order.java
+++ pulsedelivery/src/main/java/br/marcoswolf/pulsedelivery/model/Order.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 public class Order {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String customerName;
     private String address;
