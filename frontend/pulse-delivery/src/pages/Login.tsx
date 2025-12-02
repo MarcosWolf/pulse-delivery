f5e5c8daeabcdb2cc18c399de17efc5ec7f4eea5
Marcos Vinícios <viniciosramos.dev@gmail.com>
2025-12-02 11:46:36 -0300

    feat: adicionado tela de login no frontend

---
+++ frontend/pulse-delivery/src/pages/Login.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { AuthService } from "../Services/authService";
+import type { LoginRequest } from "../Services/authService";
+
+const authService = new AuthService();
+
+export const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const data: LoginRequest = { email, password };
+            const result = await authService.login(data);
+            authService.saveToken(result.token);
+            alert("Logado");
+        } catch (err) {
+            setError("Invalid credentials");
+        }
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <input placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            <button onClick={handleLogin}>Login</button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
+    );
+}
