f555447a164bc2dbb72d17154048bafcc46bd893
Marcos Vinícios <viniciosramos.dev@gmail.com>
2025-12-05 23:29:49 -0300

    chore: correção no secret do jwt

--- backend/src/main/java/br/marcoswolf/pulsedelivery/auth/JwtService.java
+++ backend/src/main/java/br/marcoswolf/pulsedelivery/auth/JwtService.java
@@ -4,6 +4,7 @@ import br.marcoswolf.pulsedelivery.model.Role;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -11,8 +12,14 @@ import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
     private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private Key getSigninKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
     public String generateToken(Long id, String email, Role role) {
         return Jwts.builder()
@@ -21,18 +28,25 @@ public class JwtService {
                 .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key)
+                .signWith(getSigninKey())
                 .compact();
     }
 
     public String extractEmail(String token) {
-        return Jwts.parserBuilder().setSigningKey(key).build()
-                .parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigninKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
     public boolean isTokenValid(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigninKey())
+                    .build()
+                    .parseClaimsJws(token);
             return true;
         } catch (Exception e) {
             return false;
